8cc861911f2739e6837a6e15ecd3dd24e098aaca
Eduardo Silva Xavier <109026671+eDusVx@users.noreply.github.com>
2024-07-11 15:16:58 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     app: {
-        token: "MTIzMTQzODkwNzc0OTQzNzUzMA.GENYqW.Vl4kz08Kd6l0ZSENyrWi9E5Oiii8Bwx-sEfrz8",
+        token: process.env.TOKEN,
         playing: 'Enzo 👍',
         global: true,
         guild: false,
