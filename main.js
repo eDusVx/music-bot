c86e153d7a117bb0484baed69029d0def894c1cb
Eduardo Xavier <109026671+eDusVx@users.noreply.github.com>
2024-09-13 23:14:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ client.config = require('./config');
 
 const player = new Player(client, client.config.opt.discordPlayer);
 global.genius = new Genius.Client();
+player.extractors.loadDefault((ext) => ext !== "YouTubeExtractor");
 player.extractors.register(YoutubeiExtractor, {})
 
 require('./src/loader');
