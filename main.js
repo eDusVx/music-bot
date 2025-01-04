5fedba67ef8ae025ec4d38b5e86ce6f1bf6ea60c
Eduardo Xavier <109026671+eDusVx@users.noreply.github.com>
2025-01-04 10:52:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { Player } = require('discord-player');
 const Genius = require("genius-lyrics");
 const { Client, GatewayIntentBits } = require('discord.js');
-const { YoutubeiExtractor } = require('discord-player-youtubei')
+const { DefaultExtractors } = require('@discord-player/extractor');
 
 global.client = new Client({
     intents: [
@@ -18,8 +18,10 @@ client.config = require('./config');
 
 const player = new Player(client, client.config.opt.discordPlayer);
 global.genius = new Genius.Client();
-player.extractors.loadDefault((ext) => ext !== "YouTubeExtractor");
-player.extractors.register(YoutubeiExtractor, {})
+
+(async () => {
+    await player.extractors.loadMulti(DefaultExtractors);
+})();
 
 require('./src/loader');
 
