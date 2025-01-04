b20102f37e16587b937e2ab0a719ed31f1f429fe
Eduardo Xavier <109026671+eDusVx@users.noreply.github.com>
2025-01-04 11:07:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,11 +21,15 @@ const player = new Player(client, client.config.opt.discordPlayer);
 global.genius = new Genius.Client();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+    res.send('Hello World!')
+  })
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 (async () => {
     await player.extractors.loadMulti(DefaultExtractors);
