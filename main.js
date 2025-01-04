49bc68ff539afef1d08637fbcba2a306834e6683
Eduardo Xavier <109026671+eDusVx@users.noreply.github.com>
2025-01-04 11:02:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { Player } = require('discord-player');
 const Genius = require("genius-lyrics");
 const { Client, GatewayIntentBits } = require('discord.js');
 const { DefaultExtractors } = require('@discord-player/extractor');
+const express = require('express');
 
 global.client = new Client({
     intents: [
@@ -18,6 +19,13 @@ client.config = require('./config');
 
 const player = new Player(client, client.config.opt.discordPlayer);
 global.genius = new Genius.Client();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 (async () => {
     await player.extractors.loadMulti(DefaultExtractors);
